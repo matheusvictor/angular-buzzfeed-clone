83c15a9d956dc489fc664b238f0d0bd36a8bbb6d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-22 19:45:30 -0300

    feat: Add regras do quizz

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+[x: string]: any;
+  
+  title: string = '';
+
+  questions : any;
+  questionSelected: any;
+
+  answers: string[] = [];
+  answersSelected: string = '';
+
+  questionIndex: number = 0;
+  questionMaxIndex : number = 0;
+
+  gameFinished = false;
+
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.gameFinished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string): void {
+    this.answers.push(value);
+  }
+
+  async nextStep() {
+    this.questionIndex++;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];  
+    } else {
+      this.gameFinished = true;
+    }
+
+  }
 
 }
